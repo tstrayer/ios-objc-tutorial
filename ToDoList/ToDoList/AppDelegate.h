f9cfa5651656228c78d8caeff5ca8189b96a2da2
Ty Strayer <tstrayer@ford.com>
2016-02-21 19:57:57 -0500

    Initial commit of iOS To Do List tutorial in Obj-C.

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Ty on 2/21/16.
+//  Copyright © 2016 Ty Strayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ToDoList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ToDoList
+//
+//  Created by Ty on 2/21/16.
+//  Copyright © 2016 Ty Strayer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
